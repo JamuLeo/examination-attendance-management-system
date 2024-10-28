d0ef780ae22b5476b71c2a6cf19f28ed2733d492
JamuLeo <bsc-com-06-21@unima.ac.mw>
2024-10-27 19:12:10 -0700

    initialize project using create-expo-app

---
+++ app/Next.js
@@ -0,0 +1,29 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router'; 
+//import Name from '../components/Name';
+export default function Next() {
+  return (
+    <View  style={styles.container}>
+      <Text style={styles.text}>next component here</Text>
+       
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
---
+++ components/Name.js
@@ -0,0 +1,15 @@
+import {Text} from "react-native";
+
+const Name =() => {
+
+    return(
+
+
+        <Text>My name is john</Text>
+    );
+  
+
+};
+
+
+export default Name;
